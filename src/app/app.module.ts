e77de7bcbb36162fc02b0a7a99cf155e8fedd2aa
Alexanderhuxel <Alexanderhuxel@gmail.com>
2021-03-27 12:08:56 +0100

    added all sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { LandComponent } from './land/land.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandComponent,
+    ContactComponent,
+    FooterComponent,
+    PortfolioComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
