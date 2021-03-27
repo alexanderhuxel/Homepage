59515d4d1988ca679387fe5d684012de34aedaf2
Alexanderhuxel <Alexanderhuxel@gmail.com>
2021-03-27 15:14:28 +0100

    edit some style

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.sass']
 })
 export class PortfolioComponent implements OnInit {
-
+  portfolio = [{
+    name: "Quiz",
+    img: "assets/img/Quiz.png",
+    tec: ["assets/img/css.svg", "assets/img/html.svg", "assets/img/javascript.svg"]
+  }, {
+    name: "Tic Tac Toe",
+    img: "assets/img/tictactoe.png",
+    tec: ["assets/img/css.svg", "assets/img/html.svg", "assets/img/javascript.svg"]
+  }, {
+    name: "Homepage",
+    img: "assets/img/homepage.png",
+    tec: ["assets/img/angular.svg", "assets/img/sass.svg", "assets/img/html.svg", "assets/img/typescript.svg", "assets/img/javascript.svg"]
+  }, {
+    name: "Ring Of Fire",
+    img: "assets/img/ringoffire.png",
+    tec: ["assets/img/angular.svg", "assets/img/sass.svg", "assets/img/html.svg", "assets/img/typescript.svg", "assets/img/javascript.svg"]
+  }]
   constructor() { }
 
   ngOnInit(): void {
